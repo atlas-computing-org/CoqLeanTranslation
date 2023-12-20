9fac0bc50ac92434e0efd1a32e1e2644803074b6
Daniel Windham <daniel@atlascomputing.org>
2023-12-20 16:11:57 -0500

    lean: [ChatGPT-3.5] Translate Toy ISA implementation from Coq

---
+++ CoqLeanTranslation/Lean/ToyISA.lean
@@ -0,0 +1,200 @@
+namespace ToyISA
+
+open nat
+
+structure Registers :=
+  (accumulator instruction_register program_counter memory_address_register memory_buffer_register : ℕ)
+
+def Memory := ℕ → ℕ
+
+inductive Instruction
+| LOAD (addr : ℕ)
+| STORE (addr : ℕ)
+| ADD (addr : ℕ)
+| SUB (addr : ℕ)
+| JMP (addr : ℕ)
+| JZ (addr : ℕ)
+| NOP
+| HALT
+
+def decode_instruction (regs : Registers) (mem : Memory) : Instruction :=
+  let encoded_instr := regs.instruction_register in
+  match encoded_instr with
+  | 0 => Instruction.NOP
+  | 1 => Instruction.HALT
+  | 2 => Instruction.LOAD (mem (regs.program_counter + 1))
+  | 3 => Instruction.STORE (mem (regs.program_counter + 1))
+  | 4 => Instruction.ADD (mem (regs.program_counter + 1))
+  | 5 => Instruction.SUB (mem (regs.program_counter + 1))
+  | 6 => Instruction.JMP (mem (regs.program_counter + 1))
+  | 7 => Instruction.JZ (mem (regs.program_counter + 1))
+  | _ => Instruction.HALT
+
+def load_from_memory (regs : Registers) (mem : Memory) : Registers × Memory :=
+  let addr := regs.memory_address_register in
+  let value := mem addr in
+  ({ accumulator := regs.accumulator,
+     instruction_register := regs.instruction_register,
+     program_counter := regs.program_counter,
+     memory_address_register := regs.memory_address_register,
+     memory_buffer_register := value }, mem)
+
+def store_to_memory (regs : Registers) (mem : Memory) : Registers × Memory :=
+  let addr := regs.memory_address_register in
+  let value := regs.memory_buffer_register in
+  let new_mem := λ n, if n = addr then value else mem n in
+  (regs, new_mem)
+
+def execute_load_from_memory_subinstruction (addr : ℕ) (regs : Registers) (mem : Memory) : Registers × Memory :=
+  let regs_with_mar :=
+    { accumulator := regs.accumulator,
+      instruction_register := regs.instruction_register,
+      program_counter := regs.program_counter,
+      memory_address_register := addr,
+      memory_buffer_register := regs.memory_buffer_register } in
+  load_from_memory regs_with_mar mem
+
+def execute_instruction (instr : Instruction) (regs : Registers) (mem : Memory) : Registers × Memory :=
+  match instr with
+  | Instruction.LOAD addr =>
+    let (regs_loaded, mem_loaded) := execute_load_from_memory_subinstruction addr regs mem in
+    ({ accumulator := regs_loaded.memory_buffer_register,
+       instruction_register := regs_loaded.instruction_register,
+       program_counter := regs_loaded.program_counter + 1,
+       memory_address_register := regs_loaded.memory_address_register,
+       memory_buffer_register := regs_loaded.memory_buffer_register }, mem_loaded)
+  | Instruction.STORE addr =>
+    let regs_with_mar_mbr :=
+      { accumulator := regs.accumulator,
+        instruction_register := regs.instruction_register,
+        program_counter := regs.program_counter,
+        memory_address_register := addr,
+        memory_buffer_register := regs.accumulator } in
+    let (regs_stored, mem_stored) := store_to_memory regs_with_mar_mbr mem in
+    ({ accumulator := regs_stored.accumulator,
+       instruction_register := regs_stored.instruction_register,
+       program_counter := regs_stored.program_counter + 1,
+       memory_address_register := regs_stored.memory_address_register,
+       memory_buffer_register := regs_stored.memory_buffer_register }, mem_stored)
+  | Instruction.ADD addr =>
+    let (regs_loaded, mem_loaded) := execute_load_from_memory_subinstruction addr regs mem in
+    ({ accumulator := regs_loaded.accumulator + regs_loaded.memory_buffer_register,
+       instruction_register := regs_loaded.instruction_register,
+       program_counter := regs_loaded.program_counter + 1,
+       memory_address_register := regs_loaded.memory_address_register,
+       memory_buffer_register := regs_loaded.memory_buffer_register }, mem_loaded)
+  | Instruction.SUB addr =>
+    let (regs_loaded, mem_loaded) := execute_load_from_memory_subinstruction addr regs mem in
+    ({ accumulator := regs_loaded.accumulator - regs_loaded.memory_buffer_register,
+       instruction_register := regs_loaded.instruction_register,
+       program_counter := regs_loaded.program_counter + 1,
+       memory_address_register := regs_loaded.memory_address_register,
+       memory_buffer_register := regs_loaded.memory_buffer_register }, mem_loaded)
+  | Instruction.JMP addr =>
+    ({ accumulator := regs.accumulator,
+       instruction_register := regs.instruction_register,
+       program_counter := addr,
+       memory_address_register := regs.memory_address_register,
+       memory_buffer_register := regs.memory_buffer_register }, mem)
+  | Instruction.JZ addr =>
+    if regs.accumulator = 0 then
+      ({ accumulator := regs.accumulator,
+         instruction_register := regs.instruction_register,
+         program_counter := addr,
+         memory_address_register := regs.memory_address_register,
+         memory_buffer_register := regs.memory_buffer_register }, mem)
+    else
+      ({ accumulator := regs.accumulator,
+         instruction_register := regs.instruction_register,
+         program_counter := regs.program_counter + 1,
+         memory_address_register := regs.memory_address_register,
+         memory_buffer_register := regs.memory_buffer_register }, mem)
+  | Instruction.NOP =>
+    ({ accumulator := regs.accumulator,
+       instruction_register := regs.instruction_register,
+       program_counter := regs.program_counter + 1,
+       memory_address_register := regs.memory_address_register,
+       memory_buffer_register := regs.memory_buffer_register }, mem)
+  | Instruction.HALT => (regs, mem)
+  end
+
+def fetch_instruction (regs : Registers) (mem : Memory) : Registers :=
+  let pc := regs.program_counter in
+  let encoded_instr := mem pc in
+  { accumulator := regs.accumulator,
+    instruction_register := encoded_instr,
+    program_counter := regs.program_counter,
+    memory_address_register := regs.memory_address_register,
+    memory_buffer_register := regs.memory_buffer_register }
+
+inductive TerminationStatus
+| NormalTermination
+| FuelExhausted
+
+def execute_single_cycle (regs : Registers) (mem : Memory) : Registers × Memory :=
+  let fetched_regs := fetch_instruction regs mem in
+  let instr := decode_instruction fetched_regs mem in
+  let pc_increment :=
+    match instr with
+    | Instruction.LOAD _ | Instruction.STORE _ | Instruction.ADD _ | Instruction.SUB _ | Instruction.JMP _ | Instruction.JZ _ => 2
+    | Instruction.NOP | Instruction.HALT => 1
+    end in
+  let updated_regs :=
+    { accumulator := fetched_regs.accumulator,
+      instruction_register := fetched_regs.instruction_register,
+      program_counter := fetched_regs.program_counter + pc_increment,
+      memory_address_register := fetched_regs.memory_address_register,
+      memory_buffer_register := fetched_regs.memory_buffer_register } in
+  match instr with
+  | Instruction.HALT => (updated_regs, mem)
+  | _ => execute_instruction instr updated_regs mem
+
+def execute_cycles (regs : Registers) (mem : Memory) (fuel : ℕ) : Registers × Memory × TerminationStatus :=
+  match fuel with
+  | 0 => (regs, mem, TerminationStatus.FuelExhausted)
+  | succ fuel' :=
+    let (new_regs, new_mem) := execute_single_cycle regs mem in
+    match new_regs.instruction_register with
+    | 1 => (new_regs, new_mem, TerminationStatus.NormalTermination)
+    | _ => execute_cycles new_regs new_mem fuel'
+    end
+  end
+
+def program_halts (initial_regs : Registers) (mem : Memory) (fuel : ℕ) : bool :=
+  let (_, _, status) := execute_cycles initial_regs mem fuel in
+  match status with
+  | TerminationStatus.NormalTermination => true
+  | TerminationStatus.FuelExhausted => false
+  end
+
+end ToyISA
+
+namespace ExampleProgram
+
+open ToyISA
+
+def example_regs : Registers :=
+  { accumulator := 0,
+    instruction_register := 0,
+    program_counter := 0,
+    memory_address_register := 0,
+    memory_buffer_register := 0 }
+
+def example_memory : Memory :=
+  λ addr,
+    match addr with
+    | 0 => 0
+    | 1 => 2
+    | 2 => 5
+    | 3 => 6
+    | 4 => 6
+    | 5 => 42
+    | 6 => 1
+    | _ => 0
+
+def example_fuel_limit : ℕ := 10
+
+def example_program_halts : bool :=
+  program_halts example_regs example_memory example_fuel_limit
+
+end ExampleProgram
